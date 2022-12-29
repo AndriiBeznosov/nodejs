2d4d2faf0e6789bc793ab3860579b80512b4453b
Andrii Beznosov <andrey301288@gmail.com>
2022-12-29 23:11:05 +0200

    lesson-2

---
+++ lesson-2/examples/06-todo-cli/todo.js
@@ -0,0 +1,15 @@
+//*-------------------------
+//* node todo.js add <title>
+//* node todo.js remove <id>
+//* node todo.js list
+//*-------------------------
+
+function invokeAction({ action }) {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+}
